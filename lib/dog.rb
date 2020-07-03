e266af52e079437cc3f1dcbb46d463abf77b5783
Sydney Jenkins <sydjenkins168@gmail.com>
2020-07-03 16:43:44 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+  def initialize(breed)
+    @breed=breed
+  end 
+  
+  def breed(breed)
+    @breed=breed
+  end 
+  
+  def breed
+    if @breed 
+      return @breed
+    else
+      return "Mutt"
+    end 
+  end 
