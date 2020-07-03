03ebf31fcfddef01bf309bb473e081641f235d84
Sydney Jenkins <sydjenkins168@gmail.com>
2020-07-03 16:46:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,18 @@
-class Dog 
-  def initialize(breed)
-    @breed=breed
+class Dog
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name
+    @name
+  end
+  def breed
+    @breed
   end 
-  
-  def breed(breed)
-    @breed=breed
-  end 
-  
-  
+end
